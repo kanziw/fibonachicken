d0cbc623ff088dd2b284cc14e2be1c4e4a9d3c2c
kanziw <kanziwoong@gmail.com>
2022-06-11 02:05:52 +0900

    Add links

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,13 @@ const App = () => (
     <p>일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,</p>
     <p>내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.</p>
     <br />
-    <p>출처: https://fibonachicken.herokuapp.com/</p>
+    <>
+      Links
+      <ul>
+        <li>기여: <a href="https://github.com/kanziw/fibonachicken" target="_blank" rel="noreferrer">https://github.com/kanziw/fibonachicken</a></li>
+        <li>출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a></li>
+      </ul>
+    </>
   </>
 )
 
