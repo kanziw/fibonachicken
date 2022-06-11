f2d77fe1a77df8c825efebc8d4fd0fdcf3c245d8
kanziw <kanziwoong@gmail.com>
2022-06-11 21:43:54 +0900

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ const styles: Record<string, CSSProperties> = {
   main: {
     margin: '1.5rem auto',
     width: '80%',
-    lineHeight: '5rem',
   },
-  section: {
+  calculator: {
     textAlign: 'center',
     fontSize: '1.8rem',
+    lineHeight: '5rem',
   },
   input: {
     maxWidth: '50%',
@@ -22,7 +22,11 @@ const styles: Record<string, CSSProperties> = {
     cursor: 'pointer',
   },
   chickens: {
-    lineHeight: '2rem',
+    textAlign: 'center',
+    fontSize: '1.8rem',
+  },
+  description: {
+    lineHeight: '1.5rem',
   },
 }
 
@@ -63,7 +67,7 @@ const App = () => {
       <Header>피보나치킨 계산기</Header>
       <HorizontalDevider />
       <main style={styles.main}>
-        <section style={styles.section}>
+        <section style={styles.calculator}>
           <input
             style={styles.input}
             inputMode="numeric"
@@ -75,21 +79,23 @@ const App = () => {
           {errMessage ? '🤔' : chicken}닭! 🐔
           <span style={styles.arrow} onClick={increase(1)}>&#9650;</span>
           <span style={styles.arrow} onClick={increase(-1)}>&#9660;</span>
-          <p style={styles.chickens}>
-            {errMessage || (chicken < 10000 ? '🐔'.repeat(chicken) : '이정도면 🐔이 모자라지 않을까요...?')}
-          </p>
+        </section>
+        <section style={styles.chickens}>
+          <p>{errMessage || (chicken < 10000 ? '🐔'.repeat(chicken) : '이정도면 🐔이 모자라지 않을까요...?')}</p>
         </section>
         <br />
-        <details>
-          <summary>세상 만사..</summary>
-          <p>
-            모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.<br />
-            일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,<br />
-            내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.<br />
-            <br />
-            출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a>
-          </p>
-        </details>
+        <section style={styles.description}>
+          <details>
+            <summary>세상 만사..</summary>
+            <p>
+              모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.<br />
+              일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,<br />
+              내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.<br />
+              <br />
+              출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a>
+            </p>
+          </details>
+        </section>
       </main>
     </>
   )
