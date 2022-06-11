0d516b566d43ccfafa118dffe754451031b5f342
kanziw <kanziwoong@gmail.com>
2022-06-11 17:04:13 +0900

    Add details

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,16 @@ const App = () => {
       <input type="number" defaultValue={people} onChange={onChange} />
       <p>{errMessage || `${people}명이면 ${chicken}닭이죠!`}</p>
       <br />
-      <p>
-        세상 만사 모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.<br />
-        일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,<br />
-        내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.
-      </p>
-      <br />
-      <>
-        Links
-        <ul>
-          <li>기여: <a href="https://github.com/kanziw/fibonachicken" target="_blank" rel="noreferrer">https://github.com/kanziw/fibonachicken</a></li>
-          <li>출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a></li>
-        </ul>
-      </>
+      <details>
+        <summary>더 알아보기</summary>
+        <p>
+          세상 만사 모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.<br />
+          일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,<br />
+          내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.
+          <br />
+          출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a>
+        </p>
+      </details>
     </>
   )
 }
