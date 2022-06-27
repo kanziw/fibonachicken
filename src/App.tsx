c91e38d1a785e9c079940eba5d290daa20054cfd
kanziw <kanziwoong@gmail.com>
2022-06-27 11:41:39 +0900

    Add author twitter

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/app.css'
 import { CSSProperties, useState } from 'react'
 import { useDoubleTap } from 'use-double-tap'
 
+import { ReactComponent as Twitter } from './assets/twitter.svg'
 import { Debugger, Header, HorizontalDevider } from './components'
 import { useFibonaChicken } from './fibonaChicken'
 
@@ -30,6 +31,14 @@ const styles: Record<string, CSSProperties> = {
   },
   description: {
     lineHeight: '1.5rem',
+  },
+  reference: {
+    margin: '0 0.5rem',
+    fontSize: '0.8rem',
+    fontStyle: 'italic',
+  },
+  twitter: {
+    width: '1rem',
   },
 }
 
@@ -68,12 +77,12 @@ const App = () => {
         <section style={styles.description}>
           <details>
             <summary>세상 만사..</summary>
-            <p>
-              모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.<br />
-              일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,<br />
-              내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.<br />
-              <br />
-              출처: <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a>
+            <p>모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.</p>
+            <p>일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사,</p>
+            <p>내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 <strong>피보나치킨</strong> 계산기라고 부르도록 하겠다.</p>
+            <p style={styles.reference}>
+              원작자(<a href="https://twitter.com/scari_net" target="_blank" rel="noreferrer">@scari_net</a><Twitter style={styles.twitter} />)의 동의를 얻어 게시하였습니다 :)
+              from <a href="https://fibonachicken.herokuapp.com/" target="_blank" rel="noreferrer">https://fibonachicken.herokuapp.com/</a>
             </p>
           </details>
         </section>
