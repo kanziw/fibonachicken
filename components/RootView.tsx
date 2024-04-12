20072e98961f109b3ac12156a31df6a0693a7f60
Jiwoong Jung <kanziwoong@gmail.com>
2024-04-12 20:54:25 +0900

    Fix Web BackgroundColor bug (#24)

--- components/RootView.tsx
+++ components/RootView.tsx
@@ -1,12 +1,14 @@
+import { useTheme } from '@/theme';
 import type { FC } from 'react';
 import type { ViewProps } from 'react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const RootView: FC<ViewProps> = ({ children, style, ...props }) => {
+  const { backgroundColor } = useTheme();
   const insets = useSafeAreaInsets();
 
   return (
-    <SafeAreaView style={[{ marginBottom: insets.bottom }, style]} {...props}>
+    <SafeAreaView style={[{ backgroundColor, marginBottom: insets.bottom }, style]} {...props}>
       {children}
     </SafeAreaView>
   );
