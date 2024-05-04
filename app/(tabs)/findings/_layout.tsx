2a5df87188e4a961f46d6d5921d8f31236f43d03
Jiwoong Jung <kanziwoong@gmail.com>
2024-05-05 03:03:40 +0900

    Add Contacts

--- app/(tabs)/findings/_layout.tsx
+++ app/(tabs)/findings/_layout.tsx
@@ -1,19 +1,16 @@
+import { Contacts } from '@/components/Contacts';
 import { Stack } from 'expo-router';
 
 export default function FindingLayout() {
+  const headerRight = () => <Contacts />;
+
   return (
-    <Stack screenOptions={{}}>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          title: '찾기',
-        }}
-      />
-      <Stack.Screen name="fride" options={{ title: '후라이드' }} />
-      <Stack.Screen name="seasoned" options={{ title: '양념' }} />
-      <Stack.Screen name="boneless" options={{ title: '순살' }} />
-      <Stack.Screen name="grilled" options={{ title: '구운치킨' }} />
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false, title: '찾기' }} />
+      <Stack.Screen name="fride" options={{ title: '후라이드', headerRight }} />
+      <Stack.Screen name="seasoned" options={{ title: '양념', headerRight }} />
+      <Stack.Screen name="boneless" options={{ title: '순살', headerRight }} />
+      <Stack.Screen name="grilled" options={{ title: '구운치킨', headerRight }} />
     </Stack>
   );
 }
---
+++ app/contacts.tsx
@@ -0,0 +1,47 @@
+import { RootView } from '@/components/RootView';
+import { Text } from '@/components/Text';
+import { useTheme } from '@/theme';
+import { FontAwesome } from '@expo/vector-icons';
+import { Linking, StyleSheet, View } from 'react-native';
+
+export default function ContactsModal() {
+  const { foregroundColor } = useTheme();
+
+  return (
+    <RootView style={styles.container}>
+      <View style={styles.question}>
+        <Text>Q: 왜 치킨 종류가 이것밖에 없나요?</Text>
+      </View>
+      <View style={styles.answer}>
+        <Text>👉 제가 먹는 것만 먹어서 그렇습니다</Text>
+      </View>
+      <View style={styles.comments}>
+        <Text>
+          추가하고 싶은 메뉴가 있다면 <Text onPress={() => Linking.openURL('mailto: kanziwoong+fibonachicken@gmail.com')}>kanziwoong@gmail.com</Text>으로 메일 주세요 :)
+        </Text>
+        <Text>
+          혹시 엔지니어시라면 <FontAwesome name="github" size={24} color={foregroundColor} onPress={() => Linking.openURL('https://github.com/kanziw/fibonachicken')} />
+          을 통해 직접 기여해주셔도 좋습니다!
+        </Text>
+      </View>
+    </RootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 16,
+    marginHorizontal: 8,
+  },
+  question: {
+    marginVertical: 8,
+  },
+  answer: {
+    marginLeft: 8,
+    marginVertical: 8,
+  },
+  comments: {
+    marginVertical: 28,
+    marginHorizontal: 20,
+  },
+});
